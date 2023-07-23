e675407c92812f41555310702f7a7b4a8558955c
Tom Browne <tbrownex@gmail.com>
2023-07-23 20:48:07 +0000

    latest updates

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ def getConfig():
         {
             'dataLoc': '~/data/investing/tda/'
         },
-        'marketAPI':
-        {
-            "URL": 'http://api.marketstack.com/v1/',
-            "APIKey": '?access_key=ffa68edbfb08de7589a43751795e0e07',
-            'dataLoc': '/home/tbrownex/data/retirement/funds/'
-        }
+        #'marketAPI':
+        #{
+        #    "URL": 'http://api.marketstack.com/v1/',
+        #    "APIKey": '?access_key=ffa68edbfb08de7589a43751795e0e07',
+        #    'dataLoc': '/home/tbrownex/data/retirement/funds/'
+        #}
     }
